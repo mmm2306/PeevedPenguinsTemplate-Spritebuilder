57519d11f96a3538f848e71314bcf4aca6deaf2f
Mihai Motroc <mmotroc@avaya.com>
2015-03-18 16:39:23 +0200

    step2

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,17 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Mihai on 3/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+{
+    CCPhysicsNode *_physicsNode;
+    CCNode *catapultArm;
+}
+
+@end
