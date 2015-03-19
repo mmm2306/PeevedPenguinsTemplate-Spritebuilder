f46bae847057421158f9a56dc921d89b2180acd6
Mihai Motroc <mmotroc@avaya.com>
2015-03-19 09:50:10 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -11,7 +11,24 @@
 @implementation Gameplay
 {
     CCPhysicsNode *_physicsNode;
-    CCNode *catapultArm;
+    CCNode *_catapultArm;
+}
+
+-(void)didLoadFromCCB
+{
+    self.userInteractionEnabled = YES;
+}
+
+-(void)launchPenguid
+{
+    CCNode *penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
 }
 
 @end
