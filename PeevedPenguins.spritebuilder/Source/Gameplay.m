cb0d09aca2d26d6bd80d55b51eaf3e621934af2b
Mihai Motroc <mmotroc@avaya.com>
2015-03-19 12:19:23 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -21,6 +21,7 @@
     self.userInteractionEnabled = YES;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    _physicsNode.debugDraw = YES;
     
     
 }
